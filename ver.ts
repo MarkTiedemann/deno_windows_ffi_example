4315af40bc89778b6a75861b19cf845d7e9fb333
Mark Tiedemann <www.marktiedemann@gmail.com>
2021-08-22 14:08:36 +0200

    Fix result type

--- ver.ts
+++ ver.ts
@@ -1,8 +1,8 @@
 const dll = Deno.dlopen("ver.dll", {
-	CurrentMajorVersionNumber: { parameters: [], result: "i32" },
-	CurrentMinorVersionNumber: { parameters: [], result: "i32" },
-	CurrentBuildNumber: { parameters: [], result: "i32" },
-	UpdateBuildRevision: { parameters: [], result: "i32" },
+	CurrentMajorVersionNumber: { parameters: [], result: "u32" },
+	CurrentMinorVersionNumber: { parameters: [], result: "u32" },
+	CurrentBuildNumber: { parameters: [], result: "u32" },
+	UpdateBuildRevision: { parameters: [], result: "u32" },
 });
 const { CurrentMajorVersionNumber, CurrentMinorVersionNumber, CurrentBuildNumber, UpdateBuildRevision } = dll.symbols;
 console.log(`\nMicrosoft Windows [Version ${CurrentMajorVersionNumber()}.${CurrentMinorVersionNumber()}.${CurrentBuildNumber()}.${UpdateBuildRevision()}]`);
